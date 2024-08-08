389702397ac2d663a1436ba1dd2f983457b9c67d
+ <+>
2024-08-08 10:59:50 +0300

    imported navbar and transactions

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
+import { useEffect, useState } from 'react';
 import './App.css'
 import Navbar from './components/Navbar'
 import Transactions from './components/Transactions'
 
 function App() {
-  
+  const [transactions, setTransactions] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/transactions")
+      .then((res) => res.json())
+      .then((transactions) => setTransactions(transactions))
+      .catch((error) => console.log(error));
+  }, []);
 
   return (
     <>
-    <Navbar />
-    <Transactions />
+  
+  <Navbar transactions={transactions} setTransactions={setTransactions} />
+    <Transactions transactions={transactions} />
     </>
   )
 }
