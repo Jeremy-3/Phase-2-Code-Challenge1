464540591a1df2099363d2eae719ca0e17e4cde8
+ <+>
2024-08-08 13:06:01 +0300

    added a search bar in transaction

--- src/components/Transactions.jsx
+++ src/components/Transactions.jsx
@@ -3,6 +3,7 @@ import OneTrans from "./OneTrans";
 
 function Transactions() {
   const [transactions, setTransactions] = useState([])
+  const [searchQuery, setSearchQuery] = useState("");
   useEffect(()=>{
     fetch('http://localhost:3000/transactions')
     .then(res => res.json())
@@ -24,10 +25,28 @@ function Transactions() {
     })
     .catch(error => console.log("Error:", error));
   }
+const filteredTransactions = transactions.filter(transaction => transaction.description.toLowerCase().includes(searchQuery.toLowerCase())
+)
+
+const handleSearchChange = (event) => {
+  setSearchQuery(event.target.value);
+};
   console.log(transactions);
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-2xl font-bold mb-4">Transactions</h1>
+      <div>
+        <input 
+        type="text"
+        value={searchQuery}
+        onChange={handleSearchChange}
+        placeholder="Search by Description..."
+        className="p-2 border border-gray-300 rounded-lg focus:outline-none focus:border-transparent"
+        />
+      </div>
+      <ul>
+        {filteredTransactions.map((transaction)=> <OneTrans key={transaction.id} transaction={transaction} deleteTransaction={deleteTransaction}/>)}
+      </ul>
       <ul>
         {transactions.map((transaction) => (
           <OneTrans key={transaction.id} transaction={transaction} deleteTransaction={deleteTransaction}/>
